fabe69ddb639feea431bb640766e65a2840015a1
Liam Amadio <liamamadio@gmail.com>
2024-08-14 17:20:11 -0400

    blog card component

---
+++ app/components/BlogCard.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import {BlogCardProps} from '../types';
+import { Courier_Prime } from 'next/font/google';
+
+const courier = Courier_Prime({
+  subsets: ['latin'],
+  weight: '400',
+});
+export default function BlogCard({title, desc, date}: BlogCardProps) {
+  return (
+    <Link href='/' className='p-3 relative w-full bg-blue-200 rounded-lg'>
+      <h4 className='font-bold underline max-w-80'>{title}</h4>
+      <p className=''>{desc}</p>
+      <p className={`${courier.className} absolute bottom-9 right-3`}>{date}</p>
+    </Link>
+  );
+}
