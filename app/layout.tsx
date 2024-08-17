58289896a392afeb4315bdda6357a080353adf54
Liam Amadio <liamamadio@gmail.com>
2024-08-16 21:25:20 -0400

    title in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} flex flex-col space-y-6 max-w-xl ml-auto mr-auto mt-10`}
+        className={`${inter.className} flex flex-col space-y-6 max-w-xl ml-auto mr-auto mt-8`}
       >
+
+        <Link href='/'>
+          <h1 className='text-4xl underline font-bold'>liamamad.io</h1>
+        </Link>
         {children}
       </body>
     </html>
