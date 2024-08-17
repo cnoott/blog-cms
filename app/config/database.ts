e000154c720d1aa33e65e31b3858b1ec7bb831c4
Liam Amadio <liamamadio@gmail.com>
2024-08-16 21:22:57 -0400

    fixed db connection err

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,7 +1,9 @@
+import pg from 'pg';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {  
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
+  dialectModule: pg
 });
 
 export default sequelize;
