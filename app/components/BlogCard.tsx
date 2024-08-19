8d4e2025ea2a444d0bc6871ac576b5a0908b9362
Liam Amadio <liamamadio@gmail.com>
2024-08-19 16:59:38 -0400

    date formatting for blog card

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -7,11 +7,25 @@ const courier = Courier_Prime({
   weight: '400',
 });
 export default function BlogCard({title, desc, date}: BlogCardProps) {
+  const formatDate = () => {
+    var year = date.getFullYear();
+
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+
+    return month + '/' + day + '/' + year;
+  };
+
   return (
     <Link href='/' className='p-3 relative w-full bg-blue-200 rounded-lg'>
       <h4 className='font-bold underline max-w-80'>{title}</h4>
       <p className=''>{desc}</p>
-      <p className={`${courier.className} absolute bottom-9 right-3`}>{date}</p>
+      <p className={`${courier.className} absolute bottom-9 right-3`}>
+        {formatDate()}
+      </p>
     </Link>
   );
 }
