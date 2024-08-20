3cdac7926c381ca4a2c2818a3b15a14bd1fb565d
Liam Amadio <liamamadio@gmail.com>
2024-08-20 14:08:38 -0400

    Signup component

--- app/components/SignUp.tsx
+++ app/components/SignUp.tsx
@@ -1,28 +1,30 @@
 'use client';
 import { useState } from 'react';
+import { createUser } from '../auth/actions';
 
 export default function SignUpForm() {
   const [state, setState] = useState<{ message?: string; errors?: any }>({});
 
   async function handleSubmit(formData: FormData) {
-    //const result = await createPost(formData);
-    //console.log('RESULT', result);
-    //setState(result);
+    const result = await createUser(formData);
+    setState(result);
   }
 
   return (
-    <form action={handleSubmit}>
+    <form action={handleSubmit} className='w-1/2 '>
+      {state.message && <p className='text-green-600'>{state.message}</p>}
+      {state?.errors?.general && <p className='text-red-600'>{state?.errors?.general}</p>}
       <div>
-        <label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
-          Email address
+        <label htmlFor='name' className='block text-sm font-medium leading-6 text-gray-900'>
+          Username
         </label>
         <div className='mt-2'>
           <input
-            id='email'
-            name='email'
-            type='email'
+            id='name'
+            name='name'
+            type='name'
             required
-            autoComplete='email'
+            autoComplete='name'
             className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           />
         </div>
@@ -33,11 +35,6 @@ export default function SignUpForm() {
           <label htmlFor='password' className='block text-sm font-medium leading-6 text-gray-900'>
             Password
           </label>
-          <div className='text-sm'>
-            <a href='#' className='font-semibold text-indigo-600 hover:text-indigo-500'>
-              Forgot password?
-            </a>
-          </div>
         </div>
         <div className='mt-2'>
           <input
@@ -52,9 +49,27 @@ export default function SignUpForm() {
       </div>
 
       <div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor='password' className='block text-sm font-medium leading-6 text-gray-900'>
+            Super Secret Password
+          </label>
+
+        </div>
+        <div className='mt-2'>
+          <input
+            id='secretPassword'
+            name='secretPassword'
+            type='password'
+            required
+            className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          />
+        </div>
+      </div>
+
+      <div>
         <button
           type='submit'
-          className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          className='mt-5 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
           Sign in
         </button>
