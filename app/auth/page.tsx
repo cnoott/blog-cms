da2fc9d2e7c55d0cfdd510e79153fa889ef81cfe
Liam Amadio <liamamadio@gmail.com>
2024-08-20 18:12:58 -0400

    Signup title

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from '../components/SignUp';
 export default function Auth() { 
   return (
     <>
-      <h1>Signup/Signin</h1>
+      <h1 className='font-bold text-lg'>Signup</h1>
       <SignUpForm />
     </>
   );
