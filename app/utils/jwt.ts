9cb29e12a40ac2c785cc38d33a3f65ada686d59d
Liam Amadio <liamamadio@gmail.com>
2024-08-21 17:09:18 -0400

    use jose for jwt verify since crypto is not allowed on next middlewear

--- app/utils/jwt.ts
+++ app/utils/jwt.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as jose from 'jose';
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
@@ -8,6 +9,7 @@ export function generateToken(user: { id: number; name: string }): string {
   });
 }
 
-export function verifyToken(token: string) {
-  return jwt.verify(token, JWT_SECRET);
+export async function verifyToken(token: string) {
+  const encodedSecret = new TextEncoder().encode(JWT_SECRET);
+  return await jose.jwtVerify(token, encodedSecret);
 }
