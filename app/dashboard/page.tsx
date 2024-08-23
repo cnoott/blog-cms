c18926c55157b83bc59c0fdb19601ac1541675db
Liam Amadio <liamamadio@gmail.com>
2024-08-23 18:22:17 -0400

    tailwind table for posts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,88 @@
-// TODO: protected route
+import { Post } from '../models';
+import { formatDate } from '../utils/date';
+import Link from 'next/link';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const allPosts = await Post.findAll();
 
   return(
-    <p>This is the dashboard</p>
+    <div className='px-4 sm:px-6 lg:px-8'>
+      <div className='sm:flex sm:items-center'>
+        <div className='sm:flex-auto'>
+          <h1 className='text-base font-semibold leading-6 text-gray-900'>Posts</h1>
+          <p className='mt-2 text-sm text-gray-700'>
+            A list of all posts.
+          </p>
+        </div>
+        <div className='mt-4 sm:ml-16 sm:mt-0 sm:flex-none'>
+          <Link href='/dashboard/create-post'>
+            <button
+              type='button'
+              className='block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              Create Post
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className='mt-8 flow-root'>
+        <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+          <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+            <table className='min-w-full divide-y divide-gray-300'>
+              <thead>
+                <tr>
+                  <th scope='col' className='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0'>
+                    Title
+                  </th>
+                  <th scope='col' className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'>
+                    Description
+                  </th>
+                  <th scope='col' className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'>
+                    Status
+                  </th>
+                  <th scope='col' className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'>
+                    Date Published
+                  </th>
+                  <th scope='col' className='relative py-3.5 pl-3 pr-4 sm:pr-0'>
+                    <span className='sr-only'>Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className='divide-y divide-gray-200 bg-white'>
+                {allPosts.map((post) => (
+                  <tr key={post.id}>
+                    <td className='whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0'>
+                      <div className='flex items-center'>
+                        <div className='font-medium text-gray-900'>{post.title}</div>
+                      </div>
+                    </td>
+                    <td className='whitespace-nowrap px-3 py-5 text-sm text-gray-500'>
+                      <div className='mt-1 text-gray-500 text-ellipsis'>{post.desc}</div>
+                    </td>
+                    <td className='whitespace-nowrap px-3 py-5 text-sm text-gray-500'>
+                      {post.isPublished ? (
+                      <span className='inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20'>
+                        Active
+                      </span>
+                      ) : (
+                      <span className='inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-700 ring-1 ring-inset ring-yellow-600/20'>
+                        Draft
+                      </span>
+                      )}
+                    </td>
+                    <td className='whitespace-nowrap px-3 py-5 text-sm text-gray-500'>{formatDate(post.date)}</td>
+                    <td className='relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0'>
+                      <a href='#' className='text-indigo-600 hover:text-indigo-900'>
+                        Edit<span className='sr-only'>, {post.id}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
